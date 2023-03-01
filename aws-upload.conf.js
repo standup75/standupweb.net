cdf33962ab186a7f5b5b40d84f6226a24fd03274
Stan Dup <standup@Stans-MacBook-Pro.local>
2023-03-01 14:28:35 -0800

    Initial commit

---
+++ aws-upload.conf.js
@@ -0,0 +1,16 @@
+module.exports = {
+    credentials:"aws-credentials.json",
+    bucketName:"standupweb.net",
+    patterns:[
+        "css/*.*",
+        "tmp/**/*.*",
+        "images/*.*",
+        "js/*.*",
+        "*.html",
+        "*.ico",
+        "*.png",
+        "*.xml",
+        "aloro.js",
+        "manifest.json",
+    ]
+}
